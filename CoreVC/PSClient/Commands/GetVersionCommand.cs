d4b277184f87655c2f8c3c699c8f86599ec5dcd1
Rafael Castañeda Acevedo <rcastaneda@javidmexico.com>
2018-01-11 18:02:57 -0700

    Create PowerShell module project CoreVC.PSClient.

---
+++ CoreVC/PSClient/Commands/GetVersionCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace CoreVC.PSClient.Commands
+{
+    [Cmdlet(VerbsCommon.Show, "Version")]
+    public class GetVersionCommand : Cmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            var version = new Version(0, 1, 1);
+            WriteObject(version.ToString());
+        }
+    }
+}
